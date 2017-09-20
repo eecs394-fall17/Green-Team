95f6227c5f53f51aafc429386670c06690704eef
angelomatni2018 <angelomatni2018@u.northwestern.edu>
2017-09-21 00:44:59 +0100

    Template app

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, ViewChild  } from '@angular/core';
+import { Platform, MenuController, Nav } from 'ionic-angular';
+
+import { TabsPage } from '../pages/tabs/tabs';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = ListPage;
+  pages: Array<{title: string, component: any}>;
+  @ViewChild(Nav) nav: Nav;
+
+  constructor(
+    public platform: Platform, 
+    public statusBar: StatusBar, 
+    public menu: MenuController,
+    public splashScreen: SplashScreen) {
+    this.initializeApp();
+
+    // set our app's pages
+    this.pages = [
+      { title: 'My Tabs', component: TabsPage },
+      { title: 'My First List', component: ListPage }
+    ];
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(page.component);
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { Platform, MenuController, Nav } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+import { ListPage } from '../pages/list/list';
+import { ItemDetailsPage } from '../pages/item-details/item-details';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    ListPage,
+    ItemDetailsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage,
+    ListPage,
+    ItemDetailsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
