6a5ecbbb7f1034b35a26826c52dbcf052708a1b8
angelomatni2018 <angelomatni2018@u.northwestern.edu>
2017-09-22 01:47:37 +0100

    Progress populating accordion display with JSON data

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,25 +7,53 @@ import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/cont
   templateUrl: 'home.html'
 })
 export class HomePage {
-  contactlist: any;
-
+  contactlist: Array<{ 
+    displayName: string, 
+    phoneNumbers: Array<{value: string}>,
+    daytime: Date,
+    description: string,
+    open: boolean,
+    satisfied: boolean
+  }>;
   
   constructor(public navCtrl: NavController, public platform: Platform, private contacts: Contacts) {
     this.platform.ready().then(() => {
       var opts = {   
-         filter : "M",                                
-         multiple: true,        
-         hasPhoneNumber:true,                             
-         fields:  [ 'displayName', 'name' ]
-       };
-       contacts.find([ 'displayName', 'name' ],opts).then((contacts) => {
-         this.contactlist = contacts;
-       }, (error) => {
-         console.log(error);
-       })
+        filter : "M",                                
+        multiple: true,        
+        hasPhoneNumber:true,                             
+        fields:  [ 'displayName', 'name' ]
+      };
+      /*
+      contacts.find([ 'displayName', 'name' ],opts).then((contacts) => {
+        this.contactlist = contacts;
+      }, (error) => {
+        console.log(error);
+      });
+      */
+      this.contactlist = [
+        {
+          displayName: 'Mom',
+          phoneNumbers: [{value: "773-555-1929"}],
+          daytime: new Date(),
+          description: "Call soon about A",
+          open: false,
+          satisfied: false
+        },
+        {
+          displayName: 'Dad',
+          phoneNumbers: [{value: "847-555-8557"}],
+          daytime: new Date(),
+          description: "Call to talk about B",
+          open: false,
+          satisfied: false
+        }
+      ];
    })
   }
 
-  
+  toggleContactHolder(i) {
+    this.contactlist[i].open = !this.contactlist[i].open;
+  }
 
 }
